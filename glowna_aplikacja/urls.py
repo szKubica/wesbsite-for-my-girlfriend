80f5f8f01a7b1a21eb53655ebddd557623082d55
szKubica <264262@student.pwr.edu.pl>
2022-06-25 20:12:11 +0200

    commit

--- glowna_aplikacja/urls.py
+++ glowna_aplikacja/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from glowna_aplikacja.views import home, o_szkole, cennik, kontakt, nauczyciele
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', home, name='home'),
+    path('o_szkole/', o_szkole, name='o_szkole'),
+    path('cennik/', cennik, name='cennik'),
+    path('kontakt/', kontakt, name='kontakt'),
+    path('nauczyciele/', nauczyciele, name='nauczyciele')
+    ]
--- glowna_aplikacja/views.py
+++ glowna_aplikacja/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
+def home(request):
+    return render(request, 'glowna.html', {})
+
+
+def o_szkole(request):
+    return render(request, 'o_szkole.html', {})
+
+
+def cennik(request):
+    return render(request, 'cennik.html', {})
+
+
+def kontakt(request):
+    if request.method == 'POST':
+        imie = request.POST['imie']
+        szkola = request.POST['szkola']
+        email = request.POST['email']
+        wiadomosc = request.POST['wiadomosc']
+
+        wiadomosc_mail = '{} ze szkoły {} : {}'.format(imie, szkola, wiadomosc)
+
+        #wysylanie maila
+        #send_mail('Wiadomość z formularza', wiadomosc_mail, email, ['szymbarc@gmail.com'])
+
+        return render(request, 'kontakt.html', {'imie' : imie})
+    else:
+        return render(request, 'kontakt.html', {})
+
+
+def nauczyciele(request):
+    return render(request, 'nauczyciele.html', {})
 # Create your views here.
